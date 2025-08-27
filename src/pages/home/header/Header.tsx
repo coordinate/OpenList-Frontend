7863543f51a5790dc792e3270785a9aa2b8cb6bc
zx <gcoordinate@gmail.com>
2025-08-27 10:16:20 +0800

    ...

--- src/pages/home/header/Header.tsx
+++ src/pages/home/header/Header.tsx
@@ -1,25 +1,18 @@
-import {
-  HStack,
-  useColorModeValue,
-  Image,
-  Center,
-  Icon,
-  Kbd,
-  CenterProps,
-} from "@hope-ui/solid"
+import { HStack, Center, CenterProps } from "@hope-ui/solid"
 import { changeColor } from "seemly"
-import { Show, createMemo } from "solid-js"
+import {
+  Show,
+  createEffect,
+  createMemo,
+  createSignal,
+  onCleanup,
+} from "solid-js"
 import { getMainColor, getSetting, local, objStore, State } from "~/store"
-import { BsSearch } from "solid-icons/bs"
-import { CenterLoading } from "~/components"
 import { Container } from "../Container"
-import { bus } from "~/utils"
 import { Layout } from "./layout"
-import { isMac } from "~/utils/compatibility"
 
 export const Header = () => {
-  const logos = getSetting("logo").split("\n")
-  const logo = useColorModeValue(logos[0], logos.pop())
+  const [currentTime, setCurrentTime] = createSignal(new Date())
 
   const stickyProps = createMemo<CenterProps>(() => {
     switch (local["position_of_header_navbar"]) {
@@ -28,6 +21,19 @@ export const Header = () => {
       default:
         return { position: undefined, zIndex: undefined, top: undefined }
     }
+  })
+
+  createEffect(() => {
+    // 每分钟更新一次时间
+    const timer = setInterval(() => {
+      setCurrentTime(new Date())
+    })
+
+    // 组件卸载时清除定时器
+    onCleanup(() => clearInterval(timer))
+
+    // 设置初始时间
+    setCurrentTime(new Date())
   })
 
   return (
@@ -45,15 +51,7 @@ export const Header = () => {
           w="$full"
           justifyContent="space-between"
         >
-          <HStack class="header-left" h="44px">
-            <Image
-              src={logo()!}
-              h="$full"
-              w="auto"
-              fallback={<CenterLoading />}
-            />
-          </HStack>
-          <HStack class="header-right" spacing="$2">
+          <HStack class="header-left">
             <HStack
               px="$2"
               py="$1"
@@ -61,36 +59,19 @@ export const Header = () => {
               bgColor={changeColor(getMainColor(), { alpha: 0.15 })}
               color={getMainColor()}
             >
-              {new Date().toISOString()}
+              v0.0.0&nbsp;&nbsp;&nbsp;
+              {currentTime().toLocaleString("zh-CN", {
+                year: "numeric",
+                month: "2-digit",
+                day: "2-digit",
+                hour: "2-digit",
+                minute: "2-digit",
+                second: "2-digit",
+              })}
             </HStack>
           </HStack>
           <HStack class="header-right" spacing="$2">
             <Show when={objStore.state === State.Folder}>
-              <Show when={getSetting("search_index") !== "none"}>
-                <HStack
-                  bg="$neutral4"
-                  w="$32"
-                  p="$1"
-                  rounded="$md"
-                  justifyContent="space-between"
-                  border="2px solid transparent"
-                  cursor="pointer"
-                  color={getMainColor()}
-                  bgColor={changeColor(getMainColor(), { alpha: 0.15 })}
-                  _hover={{
-                    bgColor: changeColor(getMainColor(), { alpha: 0.2 }),
-                  }}
-                  onClick={() => {
-                    bus.emit("tool", "search")
-                  }}
-                >
-                  <Icon as={BsSearch} />
-                  <HStack>
-                    {isMac ? <Kbd>Cmd</Kbd> : <Kbd>Ctrl</Kbd>}
-                    <Kbd>K</Kbd>
-                  </HStack>
-                </HStack>
-              </Show>
               <Layout />
             </Show>
           </HStack>
--- src/pages/home/header/layout.tsx
+++ src/pages/home/header/layout.tsx
@@ -26,9 +26,10 @@ export const Layout = () => {
       <MenuTrigger
         as={IconButton}
         color={getMainColor()}
-        bgColor={changeColor(getMainColor(), { alpha: 0.15 })}
+        bgColor={changeColor(getMainColor(), { alpha: 0.2 })}
         _hover={{
-          bgColor: changeColor(getMainColor(), { alpha: 0.2 }),
+          // @ts-ignore
+          bgColor: changeColor(getMainColor(), { alpha: 0.5 }),
         }}
         aria-label="switch layout"
         compact
